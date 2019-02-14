504525b1aefd233b731d38ff17df3ec69e846e93
JustlPain <JustlPains@gmail.com>
2019-02-14 19:15:31 +0300

    Project preparing

---
+++ Source/BTN_ServerUS.cs
@@ -0,0 +1,18 @@
+//Fixed With [DOGE]DEN aottg Sources fixer
+//Doge Guardians FTW
+//DEN is OP as fuck.
+//Farewell Cowboy
+
+using System;
+using UnityEngine;
+
+public class BTN_ServerUS : MonoBehaviour
+{
+    private void OnClick()
+    {
+        PhotonNetwork.Disconnect();
+        PhotonNetwork.ConnectToMaster("app-us.exitgamescloud.com", 0x13bf, FengGameManagerMKII.applicationId, UIMainReferences.version);
+        FengGameManagerMKII.OnPrivateServer = false;
+    }
+}
+
---
+++ Source/BTN_Server_JPN.cs
@@ -0,0 +1,18 @@
+//Fixed With [DOGE]DEN aottg Sources fixer
+//Doge Guardians FTW
+//DEN is OP as fuck.
+//Farewell Cowboy
+
+using System;
+using UnityEngine;
+
+public class BTN_Server_JPN : MonoBehaviour
+{
+    private void OnClick()
+    {
+        PhotonNetwork.Disconnect();
+        PhotonNetwork.ConnectToMaster("app-jp.exitgamescloud.com", 0x13bf, FengGameManagerMKII.applicationId, UIMainReferences.version);
+        FengGameManagerMKII.OnPrivateServer = false;
+    }
+}
+
