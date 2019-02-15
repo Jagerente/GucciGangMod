7944ae167025be45cbfafc52a204f447a3b85c7d
JustlPain <JustlPains@gmail.com>
2019-02-15 15:12:09 +0300

    Create Settings.cs

---
+++ Source/GGP/Settings.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GGP
+{
+    class Settings
+    {
+        public static int Chat;
+        public static int RCFormatting;
+        public static string ChatMajorColor;
+        public static string ChatMinorColor;
+        public static int ChatMajorBold;
+        public static int ChatMinorBold;
+        public static int ChatMajorItalic;
+        public static int ChatMinorItalic;
+        public static string ChatSize;
+    }
+}
