5cc4c92a2878369fa048e05fdbbfeefe991fe5b3
JustlPain <JustlPains@gmail.com>
2019-02-18 14:28:47 +0300

    Get Player fix

--- Source/GGP/Utilities.cs
+++ Source/GGP/Utilities.cs
@@ -12,12 +12,15 @@ namespace GGP
 
         public static GameObject Player()
         {
-                var player = new GameObject();
-                foreach (GameObject gameObject in GameObject.FindGameObjectsWithTag("Player"))
-                {
+            var player = new GameObject();
+            foreach (GameObject gameObject in GameObject.FindGameObjectsWithTag("Player"))
+            {
+                if (IN_GAME_MAIN_CAMERA.gametype != GAMETYPE.MULTIPLAYER)
                     player = gameObject;
-                }
-                return player;
+                else if (gameObject.GetPhotonView().isMine)
+                    player = gameObject;
+            }
+            return player;
         }
     }
 }
