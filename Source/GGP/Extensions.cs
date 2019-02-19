97d9241efcd4b4abb2c6e0b412dc455d3813d46c
JustlPain <JustlPains@gmail.com>
2019-02-19 17:18:51 +0300

    Utilities.cs to Extensions.cs

---
+++ Source/GGP/Extensions.cs
@@ -0,0 +1,26 @@
+﻿using System.Text.RegularExpressions;
+using UnityEngine;
+
+namespace GGP
+{
+    class Extensions
+    {
+        public static string StripHTML(string input)
+        {
+            return Regex.Replace(input, "<.*?>", string.Empty);
+        }
+
+        public static GameObject Player()
+        {
+            var player = new GameObject();
+            foreach (GameObject gameObject in GameObject.FindGameObjectsWithTag("Player"))
+            {
+                if (IN_GAME_MAIN_CAMERA.gametype != GAMETYPE.MULTIPLAYER)
+                    player = gameObject;
+                else if (gameObject.GetPhotonView().isMine)
+                    player = gameObject;
+            }
+            return player;
+        }
+    }
+}
