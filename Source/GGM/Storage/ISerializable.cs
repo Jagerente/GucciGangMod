44ca9eba9eead490842a8d98683d54553aa68268
Jagerente <jagerente@protonmail.com>
2019-06-24 01:57:11 +1000

    Better Settings

---
+++ Source/GGM/Storage/ISerializable.cs
@@ -0,0 +1,8 @@
+﻿namespace GGM.Storage
+{
+    public interface ISerializable
+    {
+        void Deserialize(string key, IDataStorage storage);
+        void Serialize(string key, IDataStorage storage);
+    }
+}
