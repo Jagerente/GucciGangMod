cd07827abeab775d0cdd32dfdac55538c84eddca
Jagerente <jagerente@protonmail.com>
2019-06-27 16:17:14 +1000

    Optimization

--- Source/BTN_BackToMain.cs
+++ Source/BTN_BackToMain.cs
@@ -5,7 +5,7 @@ public class BTN_BackToMain : MonoBehaviour
     private void OnClick()
     {
         NGUITools.SetActive(transform.parent.gameObject, false);
-        NGUITools.SetActive(GGM.Caching.GameObjectCache.Find("UIRefer").GetComponent<UIMainReferences>().panelMain, true);
+        NGUITools.SetActive(UIMainReferences.instance.panelMain.gameObject, true);
         GGM.Caching.GameObjectCache.Find("InputManagerController").GetComponent<FengCustomInputs>().menuOn = false;
         PhotonNetwork.Disconnect();
     }
