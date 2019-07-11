64b25c4edd313d79ae26010d96d795288715c91a
Jagerente <jagerente@protonmail.com>
2019-07-11 19:18:01 +1000

    Update RichPresence.cs

--- Source/GGM/Discord/RichPresence.cs
+++ Source/GGM/Discord/RichPresence.cs
@@ -4,13 +4,15 @@ namespace GGM.Discord
 {
     class RichPresence : MonoBehaviour
     {
-        private static string[] Locations = { "akina", "annie", "cave", "city", "colossal", "forest", "house", "outside", "tutorial" };
+        private static readonly string[] Locations = { "akina", "annie", "cave", "city", "colossal", "forest", "house", "outside", "tutorial" };
 
-        private const string _clientID = "598429802692870145";
+        private const string ClientId = "598429802692870145";
 
-        private static string _largeImageKey;
+        private static string largeImageKey;
 
-        private static DiscordAPI.RichPresence _presence;
+        private static DiscordAPI.RichPresence presence;
+
+        public static string UserID;
 
         void Awake()
         {
@@ -22,8 +24,8 @@ namespace GGM.Discord
             handlers.requestCallback += (ref DiscordAPI.JoinRequest a) => { };
             handlers.spectateCallback += (a) => { };
 
-            DiscordAPI.Initialize(_clientID, ref handlers, true, null);
-            _presence = new DiscordAPI.RichPresence
+            DiscordAPI.Initialize(ClientId, ref handlers, true, null);
+            presence = new DiscordAPI.RichPresence
             {
                 details = "Main Menu",
                 state = $"Version {UIMainReferences.Version}",
@@ -34,7 +36,7 @@ namespace GGM.Discord
                 partySize = 0,
                 partyMax = 0
             };
-            DiscordAPI.UpdatePresence(_presence);
+            DiscordAPI.UpdatePresence(presence);
         }
 
         public static void Update()
@@ -48,59 +50,58 @@ namespace GGM.Discord
             {
                 if (PhotonNetwork.insideLobby)
                 {
-                    _presence.details = "Lobby";
-                    _presence.state = Extensions.GetLobbyName();
-                    _presence.partySize = 0;
-                    _presence.partyMax = 0;
+                    presence.details = "Lobby";
+                    presence.state = Extensions.GetLobbyName();
+                    presence.partySize = 0;
+                    presence.partyMax = 0;
                 }
                 else if (IN_GAME_MAIN_CAMERA.gametype == GAMETYPE.SINGLE)
                 {
-                    _presence.details = "Singleplayer";
-                    _presence.largeImageKey = GetImage();
-                    _presence.largeImageText = $"{FengGameManagerMKII.level}/{Extensions.GetDifficulty()}/{Extensions.GetDayLight()}";
-                    _presence.state = $"{FengGameManagerMKII.single_kills}/{FengGameManagerMKII.single_maxDamage}/{FengGameManagerMKII.single_totalDamage}";
-                    _presence.partySize = 0;
-                    _presence.partyMax = 0;
+                    presence.details = "Singleplayer";
+                    presence.largeImageKey = GetImage();
+                    presence.largeImageText = $"{FengGameManagerMKII.level}/{Extensions.GetDifficulty()}/{Extensions.GetDayLight()}";
+                    presence.state = $"{FengGameManagerMKII.single_kills}/{FengGameManagerMKII.single_maxDamage}/{FengGameManagerMKII.single_totalDamage}";
+                    presence.partySize = 0;
+                    presence.partyMax = 0;
                 }
                 else
                 {
-                    _presence.details = "Main Menu";
-                    _presence.state = $"Version {UIMainReferences.Version}";
-                    _presence.largeImageKey = "logo_large";
-                    _presence.largeImageText = "Art by https://vk.com/bishoptyan";
-                    _presence.partySize = 0;
-                    _presence.partyMax = 0;
+                    presence.details = "Main Menu";
+                    presence.state = $"Version {UIMainReferences.Version}";
+                    presence.largeImageKey = "logo_large";
+                    presence.largeImageText = "Art by https://vk.com/bishoptyan";
+                    presence.partySize = 0;
+                    presence.partyMax = 0;
                 }
             }
             else
             {
-                _presence.details = "Multiplayer";
-                _presence.state = (Extensions.GetRoomName().Length > 14) ? (Extensions.GetRoomName().Remove(12) + "...") : Extensions.GetRoomName();
-                _presence.largeImageKey = GetImage();
-                _presence.largeImageText = $"{FengGameManagerMKII.level}/{Extensions.GetDifficulty()}/{Extensions.GetDayLight()}";
-                _presence.partySize = PhotonNetwork.room.playerCount;
-                _presence.partyMax = PhotonNetwork.room.maxPlayers;
+                presence.details = "Multiplayer";
+                presence.state = (Extensions.GetRoomName().Length > 14) ? (Extensions.GetRoomName().Remove(12) + "...") : Extensions.GetRoomName();
+                presence.largeImageKey = GetImage();
+                presence.largeImageText = $"{FengGameManagerMKII.level}/{Extensions.GetDifficulty()}/{Extensions.GetDayLight()}";
+                presence.partySize = PhotonNetwork.room.playerCount;
+                presence.partyMax = PhotonNetwork.room.maxPlayers;
             }
-            DiscordAPI.UpdatePresence(_presence);
-            Debug.Log($"-------------------------\nLargeImageKey:{_presence.largeImageKey}\nSmallImageKey{_presence.smallImageKey}\n-------------------------");
+            DiscordAPI.UpdatePresence(presence);
         }
 
         private static string GetImage()
         {
-            _largeImageKey = "logo_large";
+            largeImageKey = "logo_large";
             foreach (var location in Locations)
             {
                 if (FengGameManagerMKII.level.ToLower().Contains(location))
                 {
-                    _largeImageKey = location;
+                    largeImageKey = location;
                     break;
                 }
             }
-            if ((FengGameManagerMKII.level.ToLower().Contains("forest") || FengGameManagerMKII.level.ToLower().Contains("city")) && _largeImageKey != "logo_large")
+            if ((FengGameManagerMKII.level.ToLower().Contains("forest") || FengGameManagerMKII.level.ToLower().Contains("city")) && largeImageKey != "logo_large")
             {
-                return $"{_largeImageKey}_{Extensions.GetDayLight().ToLower()}";
+                return $"{largeImageKey}_{Extensions.GetDayLight().ToLower()}";
             }
-            return _largeImageKey;
+            return largeImageKey;
         }
     }
 }
