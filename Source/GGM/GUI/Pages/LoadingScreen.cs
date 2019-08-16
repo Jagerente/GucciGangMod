28464dc0484549831eac3a4489954da5c817b323
Jagerente <jagerente@protonmail.com>
2019-08-16 16:02:09 +1000

    Improved

--- Source/GGM/GUI/Pages/LoadingScreen.cs
+++ Source/GGM/GUI/Pages/LoadingScreen.cs
@@ -11,7 +11,7 @@ namespace GGM.GUI.Pages
             UnityEngine.GUI.Box(new Rect(0, 0, Screen.width, Screen.height), string.Empty);
             UnityEngine.GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), ColorCache.Textures[ColorCache.Black]);
             UnityEngine.GUI.DrawTexture(GUIHelpers.AlignRect(192, 192, GUIHelpers.Alignment.CENTER), Styles.Logo);
-            UnityEngine.GUI.Label(GUIHelpers.AlignRect(600, 150, GUIHelpers.Alignment.BOTTOMCENTER), "<size=64>GucciGangMod</size>\n" + "<size=32>Loading</size>", GGM.GUI.Settings.LabelStyle[1]);
+            UnityEngine.GUI.Label(GUIHelpers.AlignRect(600, 150, GUIHelpers.Alignment.BOTTOMCENTER), "GucciGangMod\n" + "Loading".SetSize(32), Styles.TextStyle(TextAnchor.MiddleCenter, FontStyle.Bold, 64, false, ColorCache.White, ColorCache.White, ColorCache.White));
         }
     }
 }
